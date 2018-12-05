3fdf3a9aca843f00ec8ca2355d39e8780dcaec38
Jeff Hall <jeffhall@gatech.edu>
2018-12-05 13:05:40 -0800

    Minor updates.

--- enhanced_server.py
+++ enhanced_server.py
@@ -1,6 +1,5 @@
 # Create a socket (connect two computers)
 import socket
-import sys
 from queue import Queue
 
 NUMBER_OF_THREADS = 2
@@ -38,3 +37,23 @@ def bind_socket():
     except socket.error as msg:
         print("Socket binding error " + str(msg) + "\n" + "Retrying...")
         bind_socket()
+
+
+# Handling connections from multiple clients and saving to a list
+# Close all previous connections where this program is re-started.
+def accepting_connections():
+    for c in all_connections:
+        c.close()
+
+    del all_connections[:]
+    del all_address[:]
+
+    while True:
+        try:
+            conn, address = s.accept()
+            s.setblocking(1)  # Prevents timeout
+            all_connections.append(conn)
+            all_address.append(address)
+            print("Connection has been established : " + address[0])
+        except:
+            print("Error accepting connections")
